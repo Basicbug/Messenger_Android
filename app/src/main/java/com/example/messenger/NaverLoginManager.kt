f2ce4bcadcc2b117e0bfd4c0098e2bbdb5f2f0e6
bsgreentea <bsgreentea1@gmail.com>
2020-05-03 21:45:23 +0900

    Implement #5 로그인 매니저 수정

--- app/src/main/java/com/example/messenger/NaverLoginManager.kt
+++ app/src/main/java/com/example/messenger/NaverLoginManager.kt
@@ -1,53 +1,91 @@
 package com.example.messenger
 
+import android.util.Log
 import android.widget.Toast
+import com.example.messenger.constants.AppInfoConstants
 import com.nhn.android.naverlogin.OAuthLogin
 import com.nhn.android.naverlogin.OAuthLoginHandler
+import io.reactivex.MaybeObserver
+import io.reactivex.Observable
+import io.reactivex.ObservableConverter
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 /**
  * @author bsgreentea
  */
-class NaverLoginManager {
 
-    companion object {
-        private val OAUTH_CLIENT_ID = "nJg0Pj0f2rEBCBQSZe6s"
-        private val OAUTH_CLIENT_SECRET = "2Ca8FHXsj4"
-        private val context = MessengerApp.applicationContext()
-        private var loginInstance: OAuthLogin? = null
+object NaverLoginManager : OAuthLoginHandler() {
 
-        fun loginInstance(): OAuthLogin? {
-            if (loginInstance == null) {
-                loginInstance = OAuthLogin.getInstance()
-                loginInstance!!.init(
-                    context,
-                    OAUTH_CLIENT_ID,
-                    OAUTH_CLIENT_SECRET,
-                    R.string.app_name.toString()
-                )
+    private const val OAUTH_CLIENT_ID = "nJg0Pj0f2rEBCBQSZe6s"
+    private const val OAUTH_CLIENT_SECRET = "2Ca8FHXsj4"
+    private val context = MessengerApp.applicationContext()
+    private var loginInstance = OAuthLogin.getInstance().apply {
+        init(
+            context,
+            OAUTH_CLIENT_ID,
+            OAUTH_CLIENT_SECRET,
+            AppInfoConstants.AppName
+        )
+    }
+
+    fun loginInstance(): OAuthLogin {
+        return loginInstance
+    }
+
+    fun naverLogout() {
+        loginInstance?.logout(context)
+    }
+
+    override fun run(success: Boolean) {
+        if (success) {
+            val accessToken = loginInstance?.getAccessToken(context).toString()
+            val refreshToken = loginInstance?.getRefreshToken(context).toString()
+            val loginState = loginInstance?.getState(context).toString()
+            Toast.makeText(context, "로그인되었습니다.", Toast.LENGTH_SHORT).show()
+        } else {
+            val errorCode = loginInstance?.getLastErrorCode(context)?.code.toString()
+            val descCode = loginInstance?.getLastErrorDesc(context).toString()
+            Toast.makeText(context, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun deleteToken() {
+
+//        Observable.just("1")
+//            .subscribeOn(Schedulers.newThread())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe(getObserver())
+
+        Schedulers.newThread().createWorker().schedule {
+            loginInstance.logoutAndDeleteToken(context)
+        }
+    }
+
+    private fun getObserver(): Observer<String> {
+        return object : Observer<String> {
+            override fun onComplete() {
+                Toast.makeText(context, "탈퇴되었습니다", Toast.LENGTH_SHORT).show()
             }
-            return loginInstance
-        }
 
-        fun naverLogout() {
-            loginInstance()!!.logout(context)
-        }
+            override fun onSubscribe(d: Disposable) {
+            }
 
-        val naverLoginHandler: OAuthLoginHandler = object : OAuthLoginHandler() {
+            override fun onNext(t: String) {
+                loginInstance.logoutAndDeleteToken(context)
+                val errorCode = loginInstance?.getLastErrorCode(context)?.code.toString()
+                val descCode = loginInstance?.getLastErrorDesc(context).toString()
+                Log.d("delete_error", errorCode + " / " + descCode)
+            }
 
-            override fun run(success: Boolean) {
-
-                if (success) {
-                    val accessToken = loginInstance()!!.getAccessToken(context).toString()
-                    val refreshToken = loginInstance()!!.getRefreshToken(context).toString()
-                    val loginState = loginInstance()!!.getState(context).toString()
-                    Toast.makeText(context, "로그인되었습니다.", Toast.LENGTH_SHORT).show()
-                } else {
-                    val errorCode: String = loginInstance()!!.getLastErrorCode(context).code
-                    val descCode: String = loginInstance()!!.getLastErrorDesc(context)
-                    Toast.makeText(context, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
-                }
+            override fun onError(e: Throwable) {
+//                Toast.
             }
         }
-
     }
 }
