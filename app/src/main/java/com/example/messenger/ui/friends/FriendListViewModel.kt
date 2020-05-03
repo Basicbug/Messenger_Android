d3e9d3408843d1fa9b589ad70f9d8a0ec1bd98e8
ChoMk <cmk5432@naver.com>
2020-05-03 22:36:59 +0900

    Implement #10 friends 구조 잡기 - 필요한 파일 만들기

---
+++ app/src/main/java/com/example/messenger/ui/friends/FriendListViewModel.kt
@@ -0,0 +1,13 @@
+/*
+ * FriendListViewModel.kt 2020. 5. 3
+ *
+ * Copyright 2020 BasicBug. All rights Reserved.
+ *
+ */
+
+package com.example.messenger.ui.friends
+
+/**
+ * @author MyeongKi
+ */
+//TODO 여기서는 리스트 뷰에 관련된 데이터를 뽑아오고 이를 이용하여 데이터 바인딩
---
+++ app/src/main/java/com/example/messenger/ui/friends/FriendViewModelFactory.kt
@@ -0,0 +1,12 @@
+/*
+ * FriendViewModelFactory.kt 2020. 5. 3
+ *
+ * Copyright 2020 BasicBug. All rights Reserved.
+ *
+ */
+
+package com.example.messenger.ui.friends
+
+/**
+ * @author MyeongKi
+ */
