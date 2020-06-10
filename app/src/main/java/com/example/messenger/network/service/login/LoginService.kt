a5e836e9d6252b892eeb5bd8e65505badd144953
ChoMk <cmk5432@naver.com>
2020-06-10 22:10:26 +0900

    Implement #14 loginService 추가

---
+++ app/src/main/java/com/example/messenger/network/service/login/LoginService.kt
@@ -0,0 +1,25 @@
+/*
+ * LoginService.kt 2020. 6. 10
+ *
+ * Copyright 2020 BasicBug. All rights Reserved.
+ *
+ */
+
+package com.example.messenger.network.service.login
+
+import com.example.messenger.repository.model.ApiData
+import io.reactivex.Single
+import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+/**
+ * @author MyeongKi
+ */
+interface LoginService {
+    @POST("v1/social/signin/{provider}")
+    fun getJwtToken(
+        @Path("provider") provider: String,
+        @Query("accessToken") accessToken: String
+    ): Single<ApiData<String>>
+}
