03d01c95bf75084215bfbc69d5e432421870178b
bsgreentea <bsgreentea1@gmail.com>
2020-07-14 23:22:02 +0900

    Fix # 43 숨은 충돌 고침

--- app/src/main/java/com/example/messenger/ui/chat/chatroom/ChatRoomActivity.kt
+++ app/src/main/java/com/example/messenger/ui/chat/chatroom/ChatRoomActivity.kt
@@ -5,13 +5,8 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.example.messenger.R
 import com.example.messenger.base.BaseSocketActivity
-<<<<<<< HEAD:app/src/main/java/com/example/messenger/ui/chattingroom/ChattingRoomActivity.kt
-import com.example.messenger.databinding.ActivityChattingRoomBinding
-import com.example.messenger.ui.chattingroom.adapter.MessageAdapter
-=======
 import com.example.messenger.databinding.ActivityChatRoomBinding
 import com.example.messenger.ui.chat.chatroom.adapter.MessageAdapter
->>>>>>> e1f7cca0fc70cd5bf9b62f5f9c676021bec360b5:app/src/main/java/com/example/messenger/ui/chat/chatroom/ChatRoomActivity.kt
 
 /**
  * @author bsgreentea
@@ -34,22 +29,12 @@ class ChatRoomActivity : BaseSocketActivity() {
         binding.lifecycleOwner = this
 
         binding.chatRoomHelper =
-<<<<<<< HEAD:app/src/main/java/com/example/messenger/ui/chattingroom/ChattingRoomActivity.kt
-            ChattingRoomActivityHelper(
-                chattingRoomViewModel.loadMessageUseCase,
-//                "a5f4974e-bdbe-4f58-8d66-c7fd1ea4449e"
-                "1"
-            )
-=======
             ChatRoomActivityHelper(chattingRoomViewModel.loadMessageUseCase, "1")
->>>>>>> e1f7cca0fc70cd5bf9b62f5f9c676021bec360b5:app/src/main/java/com/example/messenger/ui/chat/chatroom/ChatRoomActivity.kt
-
         val messageAdapter = MessageAdapter()
         binding.recyclerview.adapter = messageAdapter
 
         subscribeMessageList(messageAdapter)
 
-<<<<<<< HEAD:app/src/main/java/com/example/messenger/ui/chattingroom/ChattingRoomActivity.kt
 //        for (i in 1..200) {
 //            val msg = Message(
 //                "1", "sender", "receiver", MessageType.MESSAGE,
@@ -58,8 +43,6 @@ class ChatRoomActivity : BaseSocketActivity() {
 //            chattingRoomViewModel.loadMessageUseCase.insertMessageToLocal(msg)
 //        }
 
-=======
->>>>>>> e1f7cca0fc70cd5bf9b62f5f9c676021bec360b5:app/src/main/java/com/example/messenger/ui/chat/chatroom/ChatRoomActivity.kt
         chattingRoomViewModel.loadMessageUseCase.loadMessages("1", 0)
 //        chattingRoomViewModel.loadMessageUseCase.loadMessages("a5f4974e-bdbe-4f58-8d66-c7fd1ea4449e", 0)
     }
