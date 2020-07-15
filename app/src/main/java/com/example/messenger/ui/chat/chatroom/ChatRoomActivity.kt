79c156dff79d41b850e2b72459aa50c1730ce4b7
bsgreentea <bsgreentea1@gmail.com>
2020-07-16 00:38:30 +0900

    Modify #43 코드 다듬기

--- app/src/main/java/com/example/messenger/ui/chat/chatroom/ChatRoomActivity.kt
+++ app/src/main/java/com/example/messenger/ui/chat/chatroom/ChatRoomActivity.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.Observer
 import com.example.messenger.R
 import com.example.messenger.base.BaseSocketActivity
 import com.example.messenger.databinding.ActivityChatRoomBinding
+import com.example.messenger.repository.model.chat.Message
 import com.example.messenger.ui.chat.chatroom.adapter.MessageAdapter
 
 /**
@@ -14,41 +15,44 @@ import com.example.messenger.ui.chat.chatroom.adapter.MessageAdapter
 class ChatRoomActivity : BaseSocketActivity() {
 
     lateinit var binding: ActivityChatRoomBinding
-    private lateinit var chattingRoomViewModel: ChatRoomViewModel
+    private lateinit var chatRoomViewModel: ChatRoomViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_chat_room)
+        val messageAdapter = MessageAdapter()
 
-        chattingRoomViewModel =
+        chatRoomViewModel =
             ChatRoomViewModelInjector.provideSampleViewModelFactory("1")
                 .create(ChatRoomViewModel::class.java)
 
-        binding.chatRoomViewModel = chattingRoomViewModel
-        binding.lifecycleOwner = this
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_chat_room)
 
-        binding.chatRoomHelper =
-            ChatRoomActivityHelper(chattingRoomViewModel.loadMessageUseCase, "1")
-        val messageAdapter = MessageAdapter()
-        binding.recyclerview.adapter = messageAdapter
+        binding.apply {
+            lifecycleOwner = this@ChatRoomActivity
+            chatRoomViewModel = this@ChatRoomActivity.chatRoomViewModel
+            recyclerview.adapter = messageAdapter
+            chatRoomHelper = ChatRoomActivityHelper(
+                this@ChatRoomActivity.chatRoomViewModel.loadMessageUseCase,
+                "1"
+            )
+        }
 
         subscribeMessageList(messageAdapter)
 
-//        for (i in 1..200) {
-//            val msg = Message(
-//                "1", "sender", "receiver", MessageType.MESSAGE,
-//                "test_msg", i.toString()
-//            )
-//            chattingRoomViewModel.loadMessageUseCase.insertMessageToLocal(msg)
-//        }
+        for (i in 1..200) {
+            val msg = Message(
+                "1", i.toString(), "1", "sender", i.toString()
+            )
+            chatRoomViewModel.loadMessageUseCase.insertMessageToLocal(msg)
+        }
 
-        chattingRoomViewModel.loadMessageUseCase.loadMessages("1", 0)
+        chatRoomViewModel.loadMessageUseCase.loadMessages("1", 0)
 //        chattingRoomViewModel.loadMessageUseCase.loadMessages("a5f4974e-bdbe-4f58-8d66-c7fd1ea4449e", 0)
     }
 
     private fun subscribeMessageList(messageAdapter: MessageAdapter) {
-        chattingRoomViewModel.messageList.observe(this, Observer {
+        chatRoomViewModel.messageList.observe(this, Observer {
             messageAdapter.submitList(it.toMutableList())
         })
     }
